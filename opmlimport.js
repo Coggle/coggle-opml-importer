790bbbeaf50924d4d1d732ef52b14349212d7f37
James Crosby <james@coggle.it>
2014-07-26 16:12:37 +0100

    use the arrange() api function instead of calculating layout manually

--- opmlimport.js
+++ opmlimport.js
@@ -14,44 +14,8 @@ function elementIsOutlineElement(el){
   return el.name().toLowerCase() === 'outline';
 }
 
-function xOffsetForChild(base_x_off, y_off, parent_height){
-  // Calculate the x-offset necessary for a child with a given vertical offset
-  var Arc_Angle = 60 * Math.PI / 180;
-  var radius = parent_height / (2 * Math.sin(Arc_Angle/2));
-  //
-  //                                                         _
-  //                                                    _-"  | |
-  //                                               _-"       |   + - - - - -
-  //                                          _-"            |   ||        |
-  //                                     _-"                 |   | |       |
-  //                                _-"              parent  |   | |       |
-  //                           _-"                  height/2 |   |  |      |
-  //                      _-"                                |   |  |      | y_off
-  //                 _-"\                                    |   |   |     |
-  //            _-"       \                                  |   |   |     |
-  //       _-"             \                                 |   |   |     |
-  //  _-"     Arc_Angle/2   |                                |   |   |     |
-  // -----------------------|--------------------------------|---|---| - - -
-  // :                                                       : dx:   :
-  // :                                                       :   :   :
-  // :                  radius * Math.cos(Arc_Angle/2)       :   :   :
-  // |-------------------------------------------------------|   :   :
-  // :                                                           :   :
-  // :          sqrt(radius*radius - y_off*y_off/4)              :   :
-  // |-----------------------------------------------------------|   :
-  // :                                                               :
-  // :                             radius                            :
-  // |---------------------------------------------------------------|
-  //
-  var dx = Math.sqrt(radius*radius - y_off*y_off) - radius * Math.cos(Arc_Angle/2);
-  
-  return base_x_off + dx;
-}
-
 function addChildrenRecursive(new_node, diagram_node, cb){
-
-  var y_off = -(new_node.estimated_height || 0) / 2;
-  var base_x_off = x_space;
+  var y_off = 0;
 
   async.eachSeries(new_node.children, function(new_child, callback){
     var text = new_child.text;
@@ -62,56 +26,31 @@ function addChildrenRecursive(new_node, diagram_node, cb){
     if(url){
       text = '[' + first_line + '](' + url + ')\n' + text.slice(first_line.length);
     }
-    // calculate a nice horizontal position for this child
-    var x_off = xOffsetForChild(base_x_off, y_off, new_node.estimated_height);
-    // place the parent item in the middle vertically compared to the child
-    // items:
-    y_off += (y_space + new_child.estimated_height) / 2;
-    diagram_node.addChild(text, {x:x_off, y:y_off}, function(err, node){
+    y_off += y_space;
+    // we don't re-arrange the whole diagram using the .arrange() function
+    // provided by the API, so this x/y positioning is only temporary
+    diagram_node.addChild(text, {x:x_space, y:y_off}, function(err, node){
       if(err)
         return callback(err);
       addChildrenRecursive(new_child, node, callback);
     });
-    y_off += (y_space + new_child.estimated_height) / 2;
-
   }, cb);
 }
 
-function estimateSizeOfText(text, level){
-  // approximate Coggle text sizes at different levels:
-  var textsize = [17, 15.3, 13, 11, 9.4, 9][Math.min(level, 5)];
-  var lines = text.split('\n').length;
-  return {
-     width: text.length * textsize, // very approximate! really we should use proper font metrics here
-    height: textsize * 1.5 * lines
-  };
-}
-
-function calculateNodeLayoutRecursive(el, level){
-  var children_total_height = 0;
+function filterNodesRecursive(el, level){
   var child_els = _.filter(el.childNodes(), elementIsOutlineElement);
   var node = {};
   var child_nodes = [];
 
   for(var i = 0; i < child_els.length; i++){
-    child_nodes.push(calculateNodeLayoutRecursive(child_els[i], level+1));
+    child_nodes.push(filterNodesRecursive(child_els[i], level+1));
   }
-  children_total_height = _.reduce(
-      _.map(child_nodes, function(n){return n.estimated_height;}),
-      function(a, b){return a + y_space + b;},
-      0
-  );
 
   node.text = el.attr('text');
   node.text = (node.text && node.text.value()) || '';
   node.url = el.attr('url');
   node.url = (node.url && node.url.value()) || '';
   node.children = child_nodes;
-
-  var own_size = estimateSizeOfText(node.text, level);
-
-  node.estimated_width  = own_size.width;
-  node.estimated_height = own_size.height + children_total_height;
 
   return node;
 }
@@ -126,11 +65,16 @@ function importOutlineToCoggle(root_el, diagram, callback){
       return callback(err);
     var diagram_root_node = nodes[0];
     
-    // add .estimated_width and .estimated_height tags for each branch
-    root_to_add = calculateNodeLayoutRecursive(root_el, 0);
+    // convert the OPML into an object representation looking like:
+    // {text:"", children:[...]}
+    root_to_add = filterNodesRecursive(root_el, 0);
 
     addChildrenRecursive(root_to_add, diagram_root_node, function(err){
+      if(err)
+        return callback(err);
+      diagram.arrange(function(err){
         callback(err, diagram.webUrl());
+      });
     });
   });
 }
@@ -140,7 +84,8 @@ exports.process = function(opml_data, acess_token, callback){
   // Create a Coggle API Client, see https://github.com/coggle/coggle-js for
   // documentation
   var coggle = new CoggleApi({
-    token: acess_token
+    token: acess_token,
+ base_url: "http://localdev.coggle.it"
   });
     
   // Parse the OPML using libxmljs
